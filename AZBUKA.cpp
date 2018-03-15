5a8dbc46a144f800928d041ae4b43cba2609a678
MMMOWWW <36041208+mmmowww@users.noreply.github.com>
2018-03-15 16:08:42 +0300

    Add files via upload

---
+++ AZBUKA.cpp
@@ -0,0 +1,234 @@
+#include <iostream>
+using namespace std;
+// Class
+int ces;
+class ALF{
+public:
+int num;
+char letter;
+//cesar();
+};
+void cesar(int ces)
+{ 
+////////
+ALF A;
+A.num = 1+ces;
+A.letter = 'A';
+//
+ALF B;
+B.num = 2+ces;
+B.letter = 'B';
+//
+ALF C;
+C.num = 3+ces;
+C.letter = 'C';
+//
+ALF D;
+D.num = 4+ces;
+D.letter = 'D';
+//
+ALF E;
+E.num = 5+ces;
+E.letter = 'E';
+//
+ALF F;
+F.num = 6+ces;
+F.letter = 'F';
+//
+ALF G;
+G.num = 7+ces;
+G.letter = 'G';
+//
+ALF H;
+H.num = 8+ces;
+H.letter = 'H';
+//
+ALF I;
+I.num = 9+ces;
+I.letter = 'I';
+//
+ALF J;
+J.num = 10+ces;
+J.letter = 'J';
+//
+ALF K;
+K.num = 11+ces;
+K.letter='K' ;
+//
+ALF L;
+L.num = 12+ces;
+L.letter = 'L';
+//
+ALF M;
+M.num = 13+ces;
+M.letter = 'M';
+//
+ALF N;
+N.num= 14+ces;
+N.letter= 'N';
+//
+ALF O;
+O.num= 15+ces;
+O.letter='O';
+//
+ALF P;
+P.num=16+ces;
+P.letter='P';
+//
+ALF Q;
+Q.num=17+ces;
+Q.letter='Q';
+//
+ALF R;
+R.num=18+ces;
+R.letter='R';
+//
+ALF S;
+S.num=19+ces;
+S.letter='S';
+//
+ALF T;
+T.num=20+ces;
+T.letter='T';
+//
+ALF U;
+U.num = 21+ces;
+U.letter= 'U';
+//
+ALF V;
+V.num= 22+ces;
+V.letter='V';
+//
+ALF W;
+W.num = 23+ces;
+W.letter = 'W';
+///
+ALF Y;
+Y.num = 24+ces;
+Y.letter='Y';
+//
+ALF Z;
+Z.num = 25+ces;
+Z.letter= 'Z';
+}
+if(ALF.num > 26)
+{
+ALF.num = 1;
+};
+
+
+///////         
+};
+//
+
+//
+int main()
+{
+//ALF::numero(int numero )
+ALF A;
+A.num = 1;
+A.letter = 'A';
+//
+ALF B;
+B.num = 2;
+B.letter = 'B';
+//
+ALF C;
+C.num = 3;
+C.letter = 'C';
+//
+ALF D;
+D.num = 4;
+D.letter = 'D';
+//
+ALF E;
+E.num = 5;
+E.letter = 'E';
+//
+ALF F;
+F.num = 6;
+F.letter = 'F';
+//
+ALF G;
+G.num = 7;
+G.letter = 'G';
+//
+ALF H;
+H.num = 8;
+H.letter = 'H';
+//
+ALF I;
+I.num = 9;
+I.letter = 'I';
+//
+ALF J;
+J.num = 10;
+J.letter = 'J';
+//
+ALF K;
+K.num = 11;
+K.letter='K' ;
+//
+ALF L;
+L.num = 12;
+L.letter = 'L';
+//
+ALF M;
+M.num = 13;
+M.letter = 'M';
+//
+ALF N;
+N.num= 14;
+N.letter= 'N';
+//
+ALF O;
+O.num= 15;
+O.letter='O';
+//
+ALF P;
+P.num=16;
+P.letter='P';
+//
+ALF Q;
+Q.num=17;
+Q.letter='Q';
+//
+ALF R;
+R.num=18;
+R.letter='R';
+//
+ALF S;
+S.num=19;
+S.letter='S';
+//
+ALF T;
+T.num=20;
+T.letter='T';
+//
+ALF U;
+U.num = 21;
+U.letter= 'U';
+//
+ALF V;
+V.num= 22;
+V.letter='V';
+//
+ALF W;
+W.num = 23;
+W.letter = 'W';
+///
+ALF Y;
+Y.num = 24;
+Y.letter='Y';
+//
+ALF Z;
+Z.num = 25;
+Z.letter= 'Z';
+
+cout << "На какое число поднять опустить шифр цезаря?";
+cin >> ces;
+
+
+
+return 0;
+}
